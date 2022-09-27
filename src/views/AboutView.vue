cbe7d6228cf3d529f1412b12e6c832e925a8445d
0xjoshva <joshuasteed123@Gmail.com>
2022-09-27 16:16:14 +0200

    to-do list

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about pa-8 pr-12">
+    <h1>Why should we use your to-do list?
+    </h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates rem quaerat autem necessitatibus consectetur natus commodi dicta mollitia laborum sunt ratione, esse perspiciatis eaque aliquam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates rem quaerat autem necessitatibus consectetur natus commodi dicta mollitia laborum sunt ratione, esse perspiciatis eaque aliquam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates rem quaerat autem necessitatibus consectetur natus commodi dicta mollitia laborum sunt ratione, esse perspiciatis eaque aliquam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates rem quaerat autem necessitatibus consectetur natus commodi dicta mollitia laborum sunt ratione, esse perspiciatis eaque aliquam?
+    </p>
+
+  </div>
+</template>
